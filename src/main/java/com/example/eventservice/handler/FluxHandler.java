4faaaa2a4a375b81ba0722bf91dbcbe6db3d7e6d
kimyeongung <kimyeongung92@gmail.com>
2023-04-01 21:59:04 +0900

    add Routers

--- src/main/java/com/example/eventservice/handler/FluxHandler.java
+++ src/main/java/com/example/eventservice/handler/FluxHandler.java
@@ -9,14 +9,16 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 @Component
 @Slf4j
 public class FluxHandler {
-    private HashMap<Object,Object> res = new HashMap<>();
-    private Mono<HashMap<Object,Object>> map = Mono.just(res);
-
     public Mono<ServerResponse> myMono(ServerRequest request){
+
+        HashMap<Object,Object> res = new HashMap<>();
+        Mono<HashMap<Object,Object>> map = Mono.just(res);
+
         // set result data json
         res.put("key1",1);
         res.put("key2",2);
@@ -27,5 +29,31 @@ public class FluxHandler {
 
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromProducer(map,HashMap.class));
+    }
+
+    public Mono<ServerResponse> getEvent(ServerRequest request){
+
+        HashMap<Object,Object> event = new HashMap<>();
+        Mono<HashMap<Object,Object>> eventmap = Mono.just(event);
+
+        log.info("getEvent");
+        event.put("event",1);
+
+        eventmap.subscribe(item->{
+            log.info("item:{}",item);
+        });
+
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromProducer(eventmap,HashMap.class));
+    }
+
+    public Mono<ServerResponse> getParam(ServerRequest request){
+        Optional<String> s = request.queryParam("param1");
+        String text = s.get();
+        Mono<String> map = Mono.just(text);
+        map.subscribe(item->log.info("param: {}",item));
+
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromProducer(map,String.class));
     }
 }
--- src/main/java/com/example/eventservice/router/RoutesConfig.java
+++ src/main/java/com/example/eventservice/router/RoutesConfig.java
@@ -1,15 +1,14 @@
 package com.example.eventservice.router;
 
 import com.example.eventservice.handler.FluxHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.*;
 
 @Configuration
+@Slf4j
 public class RoutesConfig {
     // add bean
     @Bean
@@ -17,6 +16,22 @@ public class RoutesConfig {
         return RouterFunctions.route(
                 RequestPredicates.GET("/getmap").and(RequestPredicates.accept(MediaType.APPLICATION_JSON))
                 ,handler::myMono
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> eventRoute(FluxHandler handler){
+        return RouterFunctions.route(
+                RequestPredicates.GET("/getevent").and(RequestPredicates.accept(MediaType.APPLICATION_JSON))
+                ,handler::getEvent
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> returnParam(FluxHandler handler){
+        return RouterFunctions.route(
+                RequestPredicates.GET("/reparam").and(RequestPredicates.accept(MediaType.APPLICATION_JSON))
+                ,handler::getParam
         );
     }
 }
